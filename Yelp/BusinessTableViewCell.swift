e66df74f86113ce360debcc5f9e1f55dc562b383
bis <biswa.panda@gmail.com>
2017-04-07 00:21:32 -0700

    roughly mock result cell

---
+++ Yelp/BusinessTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  BusinessTableViewCell.swift
+//  Yelp
+//
+//  Created by bis on 4/6/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class BusinessTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var reviewsLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    weak var business: Business? {
+        didSet {
+            ratingImageView.setImageWith((business?.ratingImageURL!)!)
+            thumbImage.setImageWith((business?.imageURL!)!)
+            addressLabel.text = business?.address!
+            reviewsLabel.text = "\(business?.reviewCount! ?? 0) Reviews"
+            nameLabel.text = business?.name
+            print((business?.ratingImageURL!)!)
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var businesses: [Business]!
     
+    @IBOutlet weak var businessTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        businessTableView.delegate = self
+        businessTableView.dataSource = self
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.businessTableView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
@@ -43,17 +47,23 @@ class BusinessesViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = businessTableView.dequeueReusableCell(withIdentifier: "BusinessTableViewCell",
+                                                         for: indexPath) as! BusinessTableViewCell
+        cell.business = self.businesses[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.businesses?.count ?? 0
+    }
+
     
 }
